a611cef54793b2d50db15535c6b2f7314de5212e
mehul <mailtomehulraj@gmail.com>
2025-07-11 23:07:26 +0530

    helpdesk project added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import expense_tracker from "../../assets/Projects/expense_tracker.png";
+import helpdesk from "../../assets/Projects/helpdesk.png";
 
 function Projects() {
   return (
@@ -23,6 +24,16 @@ function Projects() {
               description="Personal Money Management Tool build with react.js, express, tailwind and mongodb. Have features which allows user for add income and expenses and for better visualization they can see the chartsPersonal Money Management Tool built with React.js, Express, Tailwind and MongoDB. It has features that allow users to add income and expenses, and for better visualisation, they can see the charts."
               ghLink="https://github.com/mehulraj11/backend-mern-exepense-tracker"
               demoLink="https://expensetracker-mhvats.onrender.com/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={helpdesk}
+              isBlog={false}
+              title="Helpdesk"
+              description="A helpdesk system which is used for handling technical and operational issue related to an organization. It is based on Role-Based Access Control(RBAC) model, each role has its own permissions.  This app is only created for desktop site view and is not preferred for small screens."
+              ghLink="https://github.com/mehulraj11/weanalyz_helpdesk"
+              demoLink="https://weanalyz-helpdesk-frontend.onrender.com/"
             />
           </Col>
         </Row>
